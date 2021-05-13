2f0566b6acf02b206d315b7fac6173f90a4b7fee
Kakashi <62101864+ashwinstr@users.noreply.github.com>
2021-05-13 21:21:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ from starlette.config import Config
 
 from okta_jwt.jwt import validate_token as validate_locally
 
+import uvicorn
+import os
+
+Port = int(os.environ.get('PORT', 8080)) 
+
 
 # Load environment variables
 config = Config('.env')
@@ -114,3 +119,10 @@ def validate(token: str = Depends(oauth2_scheme)):
         return bool(res)
     except Exception:
         raise HTTPException(status_code=403)
+
+
+
+
+if __name__ == "__main__":
+    uvicorn.run("web:webX", host="0.0.0.0", port=Port, reload=True, access_log=True, log_level="info")
+
